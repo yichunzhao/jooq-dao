fd624de8910511d4f39b8768b18d94b6bdac21f0
yichunzhao <zhaoyichun@hotmail.com>
2021-05-30 20:34:14 +0200

    Initial commit

---
+++ src/main/java/com/ynz/demo/jooqdao/JooqDaoApplication.java
@@ -0,0 +1,13 @@
+package com.ynz.demo.jooqdao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JooqDaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JooqDaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ynz/demo/jooqdao/JooqDaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ynz.demo.jooqdao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JooqDaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
